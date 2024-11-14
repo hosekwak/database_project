fe378dba3f3cf96d44386738eb36e194e4deacbd
hose Kwak <podol2961@inu.ac.kr>
2024-11-14 12:28:49 +0900

    feat: first commit -Spring boot 프로젝트 빌드 -프로젝트 생성

---
+++ src/test/java/com/team/database_project/DatabaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.database_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
